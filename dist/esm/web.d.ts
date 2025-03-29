cc282e25b88d2cbb2a4f066a651cfa82eb616e22
mrbergeron83 <mr.bergeron83@gmail.com>
2025-03-28 23:11:37 -0400

    Add built files to dist folder

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,25 @@
+import { WebPlugin } from '@capacitor/core';
+import type { SocialLoginPlugin, InitializeOptions, LoginOptions, AuthorizationCode, AuthorizationCodeOptions, isLoggedInOptions, ProviderResponseMap } from './definitions';
+export declare class SocialLoginWeb extends WebPlugin implements SocialLoginPlugin {
+    private static readonly OAUTH_STATE_KEY;
+    private googleProvider;
+    private appleProvider;
+    private facebookProvider;
+    constructor();
+    private handleOAuthRedirect;
+    initialize(options: InitializeOptions): Promise<void>;
+    login<T extends LoginOptions['provider']>(options: Extract<LoginOptions, {
+        provider: T;
+    }>): Promise<{
+        provider: T;
+        result: ProviderResponseMap[T];
+    }>;
+    logout(options: {
+        provider: 'apple' | 'google' | 'facebook';
+    }): Promise<void>;
+    isLoggedIn(options: isLoggedInOptions): Promise<{
+        isLoggedIn: boolean;
+    }>;
+    getAuthorizationCode(options: AuthorizationCodeOptions): Promise<AuthorizationCode>;
+    refresh(options: LoginOptions): Promise<void>;
+}
